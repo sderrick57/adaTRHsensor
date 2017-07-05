82e3571f510427d452037d3faffaf5d4ab8c76f3
Shane Derrick <shanederrick57@gmail.com>
2017-07-05 16:06:17 -0700

    update

--- test.py
+++ test.py
@@ -24,8 +24,8 @@ while True:
 	if serlog > 100:
 			break
 
-logT = map(int, logT)
-logH = map(int, logH)
+logT = list(map(int, logT))
+logH = list(map(int, logH))
 
 avgT = sum(logT) / len(logT)
 avgH = sum(logH) / len(logH)
