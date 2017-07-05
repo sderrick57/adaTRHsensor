4cbdb4975521a5d2178c6c9c504a34e07609e47a
Shane Derrick <shanederrick57@gmail.com>
2017-07-05 15:33:31 -0700

    redo

--- test.py
+++ test.py
@@ -1,19 +1,9 @@
 import serial
 import threading
-ser = serial.Serial('/dev/ttyUSB0')
-global serlog
-serlog = []
-def serthread(cx):
-		global serlog
-		while True:
-    			if len(serlog) > 1000:
-            			serlog.pop(-1)
-        		try:
-            			s = cx.readline()
-            			serlog.insert(0,s)
-        		except:
-            			break
-		print("comms closed")
+ser = serial.Serial('/dev/ttyUSB2')
 
-t = threading.Thread(target=serthread, args=(ser,))
-t.start()
+serR = ser.readline()
+
+if serR.startswith('Temp: ') is True:
+	log = []
+	log.append(serR)
