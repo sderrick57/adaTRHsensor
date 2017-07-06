1abee92011adb978c46c101fd14b7055dc081736
Shane Derrick <shanederrick57@gmail.com>
2017-07-05 17:15:35 -0700

    b

--- test2.py
+++ test2.py
@@ -4,16 +4,16 @@ ser = serial.Serial('/dev/ttyUSB1')
 global serlog
 serlog = []
 def serthread(cx):
-	global serlog
-    while True:
-		if len(serlog) > 100:
-        	serlog.pop(-1)
-        try:
-        	s = cx.readline()
-            serlog.insert(0,s)
-        except:
-            break
-	print('comms closed')
+		global serlog
+		while True:
+				if len(serlog) > 100:
+						serlog.pop(-1)
+				try:
+ 						s = cx.readline()
+						serlog.insert(0,s)
+				except:
+						break
+		print('comms closed')
 
-t = threading.thread(target=serthread, args=(ser,))
+t = threading.Thread(target=serthread, args=(ser,))
 t.start()
