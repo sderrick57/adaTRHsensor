39fccdf84fe215f29dbacd3c3587ac82af1b02dc
Shane Derrick <shanederrick57@gmail.com>
2017-07-05 17:48:10 -0700

    h

--- test2.py
+++ test2.py
@@ -8,11 +8,11 @@ def serthread(cx):
 		while True:
 				if len(serlog) > 100:
 						serlog.pop(-1)
-				try:
- 						s = cx.readline()
-						serlog.insert(0,s)
+						try:
+ 								s = cx.readline()
+								serlog.insert(0,s)
 				except:
-						break
+							break
 		print('comms closed')
 
 t = threading.Thread(target=serthread, args=(ser,))
